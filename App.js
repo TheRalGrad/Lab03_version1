8727a65702cf44355fdafe1a5afeb361fa9e1290
TheRalGrad <fluke_fuangsuwan@hotmail.com>
2019-09-23 19:32:36 +0700

    Victory

--- App.js
+++ App.js
@@ -60,7 +60,7 @@ class App extends React.Component {
         }
         <div>Attemp {this.state.attempt}</div>
         {
-          this.state.completed && <h4>Complete</h4>
+          this.state.completed && <h4>Victory</h4>
         }
       </div>
     )
